4d56a2751aab012dc04b98d4aa09eed2a6473cd1
David Lumley <david@davidlumley.com.au>
2015-05-17 21:56:21 +1000

    add task to clean javascript and stylesheet directories

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  console.log('Simple');
+var del = require('del');
+
+
+gulp.task('default', ['clean']);
+
+gulp.task('clean', ['clean:javascripts', 'clean:stylesheets']);
+
+gulp.task('clean:javascripts', function(cb){
+  del(['public/javascripts/**/*.js'], cb);
+});
+
+gulp.task('clean:stylesheets', function(cb){
+  del(['public/stylesheets/**/*.css'], cb);
 });
