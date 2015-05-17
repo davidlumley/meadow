134d594ef1198f64e6cfc7791028670e46790bba
David Lumley <david@davidlumley.com.au>
2015-05-17 22:05:16 +1000

    add task to compile es6 to javascripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
 var gulp = require('gulp');
 
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var del = require('del');
+
+var babel = require('gulp-babel');
+
+var paths = {
+  'javascripts': 'assets/javascripts/**/*.es6',
+}
 
 
 gulp.task('default', ['clean']);
+gulp.task('build', ['build:javascripts']);
+
+gulp.task('build:javascripts', ['clean:javascripts'], function(){
+  return gulp.src(paths.javascripts)
+    .pipe(sourcemaps.init())
+      .pipe(babel())
+      .pipe(uglify())
+      .pipe(concat('application.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/javascripts'));
+});
 
 gulp.task('clean', ['clean:javascripts', 'clean:stylesheets']);
 
