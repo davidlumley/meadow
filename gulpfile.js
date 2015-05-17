cc981470641d1447673e2be8e53ac4156bacbb21
David Lumley <david@davidlumley.com.au>
2015-05-17 22:16:17 +1000

    add tasks to watch stylesheets and javascripts

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,10 @@ var paths = {
   'stylesheets': 'assets/stylesheets/**/*.scss',
 }
 
-
-gulp.task('default', ['clean', 'build']);
+gulp.task('default', ['build', 'watch']);
 gulp.task('clean', ['clean:javascripts', 'clean:stylesheets']);
 gulp.task('build', ['build:javascripts', 'build:stylesheets']);
+gulp.task('watch', ['watch:javascripts', 'watch:stylesheets']);
 
 gulp.task('build:javascripts', ['clean:javascripts'], function(){
   return gulp.src(paths.javascripts)
@@ -41,4 +41,12 @@ gulp.task('clean:javascripts', function(cb){
 
 gulp.task('clean:stylesheets', function(cb){
   del(['public/stylesheets/**/*.css'], cb);
+});
+
+gulp.task('watch:javascripts', function(){
+  gulp.watch(paths.javascripts, ['build:javascripts']);
+});
+
+gulp.task('watch:stylesheets', function() {
+  gulp.watch(paths.stylesheets, ['build:stylesheets']);
 });
